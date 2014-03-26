0efec38032fcac413633300fa82e9034a268d136
Pierre-Andre Saulais <pierre-andre@codeplay.com>
2014-03-26 17:40:35 +0000

    Simple implementation of emitEpilogue.

--- lib/Target/Leg/LegFrameLowering.cpp
+++ lib/Target/Leg/LegFrameLowering.cpp
@@ -52,38 +52,76 @@ uint64_t LegFrameLowering::computeStackSize(MachineFunction &MF) const {
   return StackSize;
 }
 
+// Materialize an offset for a ADD/SUB stack operation.
+// Return zero if the offset fits into the instruction as an immediate,
+// or the number of the register where the offset is materialized.
+static unsigned materializeOffset(MachineFunction &MF, MachineBasicBlock &MBB,
+                                  MachineBasicBlock::iterator MBBI,
+                                  unsigned Offset) {
+  const TargetInstrInfo &TII = *MF.getTarget().getInstrInfo();
+  DebugLoc dl = MBBI != MBB.end() ? MBBI->getDebugLoc() : DebugLoc();
+  const uint64_t MaxSubImm = 0xfff;
+  if (Offset <= MaxSubImm) {
+    // The stack offset fits in the ADD/SUB instruction.
+    return 0;
+  } else {
+    // The stack offset does not fit in the ADD/SUB instruction.
+    // Materialize the offset using MOVW/MOVT.
+    unsigned OffsetReg = Leg::R4;
+    unsigned OffsetLo = (unsigned)(Offset & 0xffff);
+    unsigned OffsetHi = (unsigned)((Offset & 0xffff0000) >> 16);
+    BuildMI(MBB, MBBI, dl, TII.get(Leg::MOVWi16), OffsetReg).addImm(OffsetLo)
+      .setMIFlag(MachineInstr::FrameSetup);
+    if (OffsetHi) {
+      BuildMI(MBB, MBBI, dl, TII.get(Leg::MOVTi16), OffsetReg).addReg(OffsetReg)
+        .addImm(OffsetHi).setMIFlag(MachineInstr::FrameSetup);
+    }
+    return OffsetReg;
+  }
+}
+
 void LegFrameLowering::emitPrologue(MachineFunction &MF) const {
+  // Compute the stack size, to determine if we need a prologue at all.
   const TargetInstrInfo &TII = *MF.getTarget().getInstrInfo();
   MachineBasicBlock &MBB = MF.front();
   MachineBasicBlock::iterator MBBI = MBB.begin();
   DebugLoc dl = MBBI != MBB.end() ? MBBI->getDebugLoc() : DebugLoc();
   uint64_t StackSize = computeStackSize(MF);
+  if (!StackSize)
+    return;
+
+  // Adjust the stack pointer.
   unsigned StackReg = Leg::SP;
-  const uint64_t MaxSubImm = 0xfff;
-  if (StackSize <= MaxSubImm) {
-    // The stack offset fits in the SUB instruction.
-    BuildMI(MBB, MBBI, dl, TII.get(Leg::SUB_ri), StackReg).addReg(StackReg)
-      .addImm(StackSize).setMIFlag(MachineInstr::FrameSetup);
-  } else {
-    // The stack offset does not fit in the SUB instruction.
-    // Materialize the offset using MOVW/MOVT.
-    unsigned OffsetReg = Leg::R4;
-    unsigned StackSizeLo = (unsigned)(StackSize & 0xffff);
-    unsigned StackSizeHi = (unsigned)((StackSize & 0xffff0000) >> 16);
-    BuildMI(MBB, MBBI, dl, TII.get(Leg::MOVWi16), OffsetReg).addImm(StackSizeLo)
-      .setMIFlag(MachineInstr::FrameSetup);
-    if (StackSizeHi) {
-      BuildMI(MBB, MBBI, dl, TII.get(Leg::MOVTi16), OffsetReg).addReg(OffsetReg)
-        .addImm(StackSizeLo).setMIFlag(MachineInstr::FrameSetup);
-    }
+  unsigned OffsetReg = materializeOffset(MF, MBB, MBBI, (unsigned)StackSize);
+  if (OffsetReg) {
     BuildMI(MBB, MBBI, dl, TII.get(Leg::SUB), StackReg).addReg(StackReg)
       .addReg(OffsetReg).setMIFlag(MachineInstr::FrameSetup);
+  } else {
+    BuildMI(MBB, MBBI, dl, TII.get(Leg::SUB_ri), StackReg).addReg(StackReg)
+      .addImm(StackSize).setMIFlag(MachineInstr::FrameSetup);
   }
 }
 
 void LegFrameLowering::emitEpilogue(MachineFunction &MF,
                                     MachineBasicBlock &MBB) const {
-  assert(0 && "Unimplemented");
+  // Compute the stack size, to determine if we need an epilogue at all.
+  const TargetInstrInfo &TII = *MF.getTarget().getInstrInfo();
+  MachineBasicBlock::iterator MBBI = MBB.getLastNonDebugInstr();
+  DebugLoc dl = MBBI->getDebugLoc();
+  uint64_t StackSize = computeStackSize(MF);
+  if (!StackSize)
+    return;
+
+  // Restore the stack pointer to what it was at the beginning of the function.
+  unsigned StackReg = Leg::SP;
+  unsigned OffsetReg = materializeOffset(MF, MBB, MBBI, (unsigned)StackSize);
+  if (OffsetReg) {
+    BuildMI(MBB, MBBI, dl, TII.get(Leg::ADD), StackReg).addReg(StackReg)
+      .addReg(OffsetReg).setMIFlag(MachineInstr::FrameSetup);
+  } else {
+    BuildMI(MBB, MBBI, dl, TII.get(Leg::ADD_ri), StackReg).addReg(StackReg)
+      .addImm(StackSize).setMIFlag(MachineInstr::FrameSetup);
+  }
 }
 
 // This function eliminates ADJCALLSTACKDOWN, ADJCALLSTACKUP pseudo
