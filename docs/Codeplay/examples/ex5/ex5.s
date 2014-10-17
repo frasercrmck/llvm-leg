2c0a21abbcec96f8a65639b9058c059ce06541ae
Pierre-Andre Saulais <pasaulais@free.fr>
2014-10-18 00:26:14 +0100

    Fix slides.

--- docs/Codeplay/examples/ex5/ex5.s
+++ docs/Codeplay/examples/ex5/ex5.s
@@ -1,13 +1,12 @@
 	.text
-	.file	"ex5.ll"
+	.file	"../../docs/Codeplay/examples/ex5/ex5.ll"
 	.globl	foo
 	.type	foo,@function
 foo:                                    # @foo
 # BB#0:
-	; We want to write 0x00010001 to the register.
-	; Each instruction writes half of the value (16 bits)
-	movw r1, #1  ; Write 0x00000001 (write low bits, clear high bits)
-	movt r1, #1  ; Write 0x0001XXXX (write high bits, don't touch low bits)
+	; We want to write 0x00010002 to the register.
+	movw r1, #2 ; Write 0x00000002 (write low bits, clear high bits)
+	movt r1, #1 ; Write 0x0001XXXX (write high bits, don't touch low bits)
 	add r0, r0, r1
 	bx lr
 .Ltmp0:
@@ -15,3 +14,4 @@ foo:                                    # @foo
 
 
 	.ident	"clang version 3.5.0 "
+	.section	".note.GNU-stack","",@progbits
