70c1c327de4ef6eab80e6181c8e1bc4e7aa7331b
Pierre-Andre Saulais <pasaulais@free.fr>
2014-10-18 00:36:03 +0100

    Adding DAG for MOVW/MOVT.

--- docs/Codeplay/examples/ex5/ex5.s
+++ docs/Codeplay/examples/ex5/ex5.s
@@ -4,9 +4,9 @@
 	.type	foo,@function
 foo:                                    # @foo
 # BB#0:
-	; We want to write 0x00010002 to the register.
-	movw r1, #2 ; Write 0x00000002 (write low bits, clear high bits)
-	movt r1, #1 ; Write 0x0001XXXX (write high bits, don't touch low bits)
+	; Write 0x00010002 to r1.
+	movw r1, #2 ; Write 0x00000002
+	movt r1, #1 ; Write 0x0001XXXX
 	add r0, r0, r1
 	bx lr
 .Ltmp0:
